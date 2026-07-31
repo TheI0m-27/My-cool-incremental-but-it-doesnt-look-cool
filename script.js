6d69b2fe431c15ef9b66bc153cccc403a18d65c4
Firelocked16 <utiumom047@gmail.com>
2026-07-31 11:54:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,12 @@ let sPmultiplier = 1;
 let radioReady = false;
 let autoClickMultiplier = 1;
 let supremeNeed = 1e70;
+let sPointGain = 1;
+let sPGain = 1;
+let sPupgrade5cost = 10;
+let updateUpgrade1 = false;
+let updateUpgrade2 = false;
+
 const poop = document.getElementById("poop");
 const god = document.getElementById("god");
 const poop2 = document.getElementById("poop2");
@@ -56,37 +62,16 @@ const sPupgrade5 = document.getElementById("sPupgrade5");
 const sPupgrade6 = document.getElementById("sPupgrade6");
 const sPupgrade7 = document.getElementById("sPupgrade7");
 const sPupgrade8 = document.getElementById("sPupgrade8");
+
 function updateGodText() {
-if (points > 1e70) {
-points = 1e70;
-}
-
+if (points > 1e70) points = 1e70;
 god.innerText = "You have " + Math.floor(points) + " points";
-
-if (points >= 1e6) {
-god.innerText = "You have " + points.toExponential(2) + " points";
-}
-
-if (points >= 10000 && milestone1allow === 1) {
-milestone1.style.visibility = "visible";
-}
-
-if (points >= 1e6 && milestone2poop === 1 && milestone2allow === 1) {
-milestone2.style.visibility = "visible";
-}
-
-if (points >= 1e8 && milestone3poop === 1 && milestone3allow === 1) {
-milestone3.style.visibility = "visible";
-}
-
-if (points >= 1e14 && milestone4poop === 1 && milestone4allow === 1) {
-milestone4.style.visibility = "visible";
-}
-
-if (points >= 1e30 && milestone5poop === 1 && milestone5allow === 1) {
-milestone5.style.visibility = "visible";
-}
-
+if (points >= 1e6) god.innerText = "You have " + points.toExponential(2) + " points";
+if (points >= 10000 && milestone1allow === 1) milestone1.style.visibility = "visible";
+if (points >= 1e6 && milestone2poop === 1 && milestone2allow === 1) milestone2.style.visibility = "visible";
+if (points >= 1e8 && milestone3poop === 1 && milestone3allow === 1) milestone3.style.visibility = "visible";
+if (points >= 1e14 && milestone4poop === 1 && milestone4allow === 1) milestone4.style.visibility = "visible";
+if (points >= 1e30 && milestone5poop === 1 && milestone5allow === 1) milestone5.style.visibility = "visible";
 if (points >= supremeNeed && supremeResetAllow === 1) {
 points = supremeNeed;
 supremeReset.style.visibility = "visible";
@@ -96,7 +81,6 @@ supremeReset.style.visibility = "visible";
 function reset() {
 points = 0;
 pointsAdd = 1;
-limit = 10;
 radioBoost = 3;
 speed = 1000;
 poop3buy = 0;
@@ -104,42 +88,33 @@ poop3cost = 10;
 radioReady = false;
 poop.innerText = "Press to get " + (pointsAdd * mM * sPmultiplier) + " points";
 poop3.innerText = "x2 point gain from Press for one point has 3 levels " + poop3cost + " points";
-
 radio.style.visibility = "hidden";
 radio2.style.visibility = "hidden";
 radio3.style.visibility = "hidden";
 milestone1.style.visibility = "hidden";
 milestone2.style.visibility = "hidden";
 milestone3.style.visibility = "hidden";
-
 poop2.style.visibility = "visible";
 poop3.style.visibility = "visible";
 click.style.visibility = "visible";
 click2.style.visibility = "visible";
 click3.style.visibility = "visible";
-
 updateGodText();
 }
 
 function startRadioLoop() {
-if (radioInterval) {
-clearInterval(radioInterval);
-}
-
+if (radioInterval) clearInterval(radioInterval);
 radioInterval = setInterval(function() {
 let radioChance = Math.floor(Math.random() * limit);
-
 if (radioChance === 1 && !radioReady) {
 radioReady = true;
 radio.innerText = "Clicker press to get + points x " + radioBoost + " ^0.91";
 }
-
 }, speed);
 }
 
 function autoClickRestart() {
 clearInterval(autoClickInterval);
-
 autoClickInterval = setInterval(() => {
 points += pointsAdd * mM * sPmultiplier * autoClickMultiplier;
 updateGodText();
@@ -191,11 +166,7 @@ updateGodText();
 poop.innerText = "Press to get " + (pointsAdd * mM * sPmultiplier) + " points";
 poop3.innerText = "x2 point gain from Press for 1 point has three levels cost " + poop3cost + " points";
 poop3buy += 1;
-
-if (poop3buy === 3) {
-poop3.style.visibility = "hidden";
-}
-
+if (poop3buy === 3) poop3.style.visibility = "hidden";
 }
 };
 
@@ -320,17 +291,10 @@ updateGodText();
 load.onclick = function() {
 load.disabled = true;
 updateGodText();
-
 let random = Math.floor(Math.random() * 10);
-
-if (random === 1) {
-points /= loadUnboost;
-} else {
-points *= loadBoost;
-}
-
+if (random === 1) points /= loadUnboost;
+else points *= loadBoost;
 updateGodText();
-
 setTimeout(() => {
 load.disabled = false;
 }, 60000);
@@ -338,36 +302,27 @@ load.disabled = false;
 
 function reset2() {
 reset();
-
 milestone1allow = 1;
 milestone2allow = 1;
 milestone3allow = 1;
 milestone4allow = 1;
 milestone5allow = 1;
-
 milestone2poop = 0;
 milestone3poop = 0;
 milestone4poop = 0;
 milestone5poop = 0;
-
 mM = 1;
 autoClickSpeed = 500;
-
-if (autoClickInterval) {
-clearInterval(autoClickInterval);
-}
-
+if (autoClickInterval) clearInterval(autoClickInterval);
 loadBoost = 100;
 loadUnboost = 1000;
 supremeResetAllow = 0;
-
 autoclick1.style.visibility = "hidden";
 autoclick2.style.visibility = "hidden";
 load.style.visibility = "hidden";
 milestone4.style.visibility = "hidden";
 milestone5.style.visibility = "hidden";
 supremeReset.style.visibility = "hidden";
-
 poop.innerText = "Press to get " + (pointsAdd * mM * sPmultiplier) + " points";
 updateGodText();
 }
@@ -380,12 +335,11 @@ function updateSPdisplay() {
 sPdisplay.innerText = "You have " + sP + " supreme resets";
 }
 
-let sPointGain = 1;
-let sPGain = 1;
+function updateUpgrade() {
+if (updateUpgrade1 === true) sPupgrade.innerText = "Current: * " + (sP * 1e6).toExponential(2);
+if (updateUpgrade2 === true) sPupgrade2.innerText = "Current: * " + (sP * 1e6).toExponential(2);
+}
 
-function updateUpgrade() {if (updateUpgrade1 === true) {sPupgrade.innerText = "Current: * " + (sP * 1e6).toExponential(2);} if (updateUpgrade2 === true) {sPupgrade2.innerText = "Current: * " + (sP * 1e6).toExponential(2);}} 
-
-  
 supremeReset.onclick = function() {
 reset2();
 sPoints += sPointGain;
@@ -401,28 +355,86 @@ sPupgrade5.style.visibility = "visible";
 sPupgrade6.style.visibility = "visible";
 sPupgrade7.style.visibility = "visible";
 sPupgrade8.style.visibility = "visible";
-updateSPdisplay(); updateUpgrade();
-}
-
-let updateUpgrade1 = false;
-let updateUpgrade2 = false;
+updateSPdisplay();
+updateUpgrade();
+};
 
 sPupgrade.onclick = function() {
 if (sPoints >= 1) {
-sPoints -= 1; updateUpgrade1 = true;
-sPmultiplier = sPmultiplier * (sP * 1e6);
+sPoints -= 1;
+updateUpgrade1 = true;
+sPmultiplier *= sP * 1e6;
 updateSPoints();
-sPupgrade.innerText = "Current: * " + (sP * 1e6).toExponential(2); sPupgrade.style.visibility = "hidden";
+sPupgrade.innerText = "Current: * " + (sP * 1e6).toExponential(2);
+sPupgrade.style.visibility = "hidden";
 }
 };
 
-sPupgrade2.onclick = function() {if (sPoints >= 1) {sPoints -= 1; updateUpgrade2 = true; sPmultiplier = sPmultiplier * (sP * 1e6); updateSPoints(); sPupgrade2.innerText = "Current: * " + (sP * 1e6).toExponential(2);}}
+sPupgrade2.onclick = function() {
+if (sPoints >= 1) {
+sPoints -= 1;
+updateUpgrade2 = true;
+sPmultiplier *= sP * 1e6;
+updateSPoints();
+sPupgrade2.innerText = "Current: * " + (sP * 1e6).toExponential(2);
+}
+};
 
-sPupgrade3.onclick = function() {if (sPoints >= 3) {sPoints -= 3; updateSPoints(); autoClickMultiplier = 1e6; limit = 5; sPupgrade3.style.visibility = "hidden";}}
-sPupgrade4.onclick = function() {if (sPoints >= 5) {sPoints -= 5; updateSPoints(); limit = 3; sPmultiplier *= 100; sPupgrade4.style.visibility = "hidden";}}
-let sPupgrade5cost = 10;
-sPupgrade5.onclick = function() {if (sPoints >= sPupgrade5cost) {sPupgrade5cost *= 10; sPupgrade5.innerText = "Cost + " + sPupgrade5cost.toExponential() + " sp supreme point x3 each level"; sPointGain *= 3; updateSPoints();}}
-sPupgrade6.onclick = function() {if (sPoints >= 60) {supremeNeed = 1e60; sPoints -= 60; updateSPoints(); sPupgrade6.style.visibility = "hidden";}}
-sPupgrade7.onclick = function() {if (sPoints >= 200) {supremeNeed = 1e51; sPoints -= 200; updateSPoints(); sPupgrade7.style.visibility = "hidden";}}
-  sPupgrade8.onclick = function() {if (sPoints >= 2000) {sPoints -= 2000; supremeMultiplier *= 1e6;}}
+sPupgrade3.onclick = function() {
+if (sPoints >= 3) {
+sPoints -= 3;
+updateSPoints();
+autoClickMultiplier = 1e6;
+limit = 5;
+sPupgrade3.style.visibility = "hidden";
+}
+};
+
+sPupgrade4.onclick = function() {
+if (sPoints >= 5) {
+sPoints -= 5;
+updateSPoints();
+limit = 3;
+sPmultiplier *= 100;
+sPupgrade4.style.visibility = "hidden";
+}
+};
+
+sPupgrade5.onclick = function() {
+if (sPoints >= sPupgrade5cost) {
+sPoints -= sPupgrade5cost;
+sPupgrade5cost *= 10;
+sPupgrade5.innerText = "Cost + " + sPupgrade5cost.toExponential() + " sp supreme point x3 each level";
+sPointGain *= 3;
+updateSPoints();
+}
+};
+
+sPupgrade6.onclick = function() {
+if (sPoints >= 60) {
+supremeNeed = 1e60;
+sPoints -= 60;
+updateSPoints();
+sPupgrade6.style.visibility = "hidden";
+}
+};
+
+sPupgrade7.onclick = function() {
+if (sPoints >= 200) {
+supremeNeed = 1e51;
+sPoints -= 200;
+updateSPoints();
+sPupgrade7.style.visibility = "hidden";
+}
+};
+
+sPupgrade8.onclick = function() {
+if (sPoints >= 2000) {
+sPoints -= 2000;
+sPmultiplier *= 1e6;
+updateSPoints();
+sPupgrade8.style.visibility = "hidden";
+}
+};
+
 //i originally made it on codepen so i forked the code
